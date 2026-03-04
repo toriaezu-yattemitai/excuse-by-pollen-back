c3dbc23edaa933f7aaa0bb16884fcc613469b5da
Touri Aida <touri1705@outlook.com>
2026-03-04 16:18:09 +0900

    fix : dummyのGenerationResultのuserが直っていなかった

--- src/app/api/v1/endpoints/dummy_interface.py
+++ src/app/api/v1/endpoints/dummy_interface.py
@@ -13,7 +13,7 @@ async def generate_response(req: GenerateRequest) -> GenerationResult:
         excuse="この後のMTGですが、花粉の影響で鼻水に溺れてしまっているので、少し遅れます。",
         score=60,
         id=str(uuid.uuid4().hex[:10]),
-        user_inputs=req.inputs,
+        used_inputs=req.inputs,
     )
 
 
@@ -23,5 +23,5 @@ async def retry_response(req: RetryRequest) -> GenerationResult:
         excuse="この後のMTGですが、現在、花粉前線との全面戦争の最中にあり、私の鼻腔が事実上の内海と化して鼻水の大洪水が発生しております。呼吸ひとつに命運を賭ける状況で、ティッシュ消費量は国家備蓄レベルに到達しました。必ず生還して向かいますが、到着が少々遅れます。",
         score=95,
         id=str(uuid.uuid4().hex[:10]),
-        user_inputs=req.previous_context,
+        used_inputs=req.previous_context,
     )

ab39f86956efb88b1f8ccb29c2591db83e84279c
Touri Aida <touri1705@outlook.com>
2026-03-05 17:49:29 +0900

    feature : prompt_runnerと必要なスキーマを追加

--- src/app/schemas/v2/prompt.py
+++ src/app/schemas/v2/prompt.py
@@ -2,6 +2,9 @@ from typing import Annotated
 from pydantic import BaseModel, Field, field_validator
 
 
+class PromptResult(BaseModel):
+    excuse: str
+    score: Annotated[int, Field(ge=0, le=100)]
 
 class PromptOptions(BaseModel):
     max_chars: int = Field(default=220, ge=1)
---
+++ src/app/services/v2/prompt_runner.py
@@ -0,0 +1,84 @@
+import os
+import uuid
+
+from typing import Any
+
+from dotenv import load_dotenv
+
+from google import genai
+from google.genai import types
+
+from app.services.v2.prompt_builder import generate_builder, retry_builder
+
+from app.schemas.v2.common import Inputs
+from app.schemas.v2.api import APIGenerateRequest, APIRetryRequest, APIResult
+from app.schemas.v2.prompt import PromptOptions, PromptResult
+
+from app.prompts.settings import MODEL_NAME
+
+
+# Private functions
+def _load_api_key() -> str:
+    load_dotenv()
+    
+    _api_key = os.getenv("GEMINI_API_KEY")
+
+    if not _api_key:
+        raise RuntimeError("GEMINI_API_KEY is not set. Please add it to .env.")
+    
+    return _api_key
+    
+# Public functions
+class Runner:
+    def __init__(self, *, api_key: None, model_name: None):
+        self._api_key = api_key or _load_api_key()
+        self._model_name = model_name or MODEL_NAME
+        
+        
+        self._client = genai.Client(api_key=self._api_key)
+        
+    def _push_gemini(self, prompt: str):
+        return self._client.models.generate_content(
+            model= self._model_name,
+            contents=prompt,
+            config=types.GenerateContentConfig(
+                response_mime_type="application/json",
+                response_schema=PromptResult
+            )
+        )
+        
+    def generate(self, req: APIGenerateRequest) -> APIResult:
+        
+        inputs: Inputs = req.inputs
+        options: PromptOptions = req.options
+        
+        prompt = generate_builder(inputs, options)
+
+        result = self._push_gemini(prompt)
+        
+        result_id = str(uuid.uuid4())
+        return APIResult(
+            excuse="", 
+            score=90,
+            id=result_id, 
+            used_inputs=inputs
+        )
+        
+    def retry(self, req: APIRetryRequest) -> APIResult:
+        
+        previous_context = req.previous_context
+        previous_excuse = req.previous_excuse
+        retry_instruction = req.retry_instruction
+        
+        prompt = retry_builder(previous_context, previous_excuse, retry_instruction)
+        
+        result = self._push_gemini(prompt)
+        
+        result_id = str(uuid.uuid4())
+        return APIResult(
+            excuse="", 
+            score=90,
+            id=result_id, 
+            used_inputs=previous_context
+        )
+    

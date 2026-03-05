9c2a82490e9e39927604a108b19ef7ba169b1d13
Touri Aida <touri1705@outlook.com>
2026-03-05 14:34:52 +0900

    refactor : v1スキーマの呼び出し変更

--- src/app/schemas/__init__.py
+++ src/app/schemas/__init__.py
@@ -1,5 +1,1 @@
 "Schemas package"
-
-from back.src.app.schemas.v1.prompt import PromptInputs, PromptOptions, PromptRequest
-
-__all__ = ["PromptInputs", "PromptOptions", "PromptRequest"]
--- src/app/schemas/v1/__init__.py
+++ src/app/schemas/v1/__init__.py
@@ -1,1 +1,5 @@
-"Schemas v1"
+"Schemas v1 package"
+
+from app.schemas.v1.prompt import PromptInputs, PromptOptions, PromptRequest
+
+__all__ = ["PromptInputs", "PromptOptions", "PromptRequest"]

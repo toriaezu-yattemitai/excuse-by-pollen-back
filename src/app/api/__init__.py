f3e9fa129980c40b583611b8e82066f19d1a8554
Touri Aida <touri1705@outlook.com>
2026-03-08 12:37:14 +0900

    refactor : ルーティング調整

--- src/app/api/__init__.py
+++ src/app/api/__init__.py
@@ -2,13 +2,10 @@
 
 from fastapi import APIRouter
 
-from app.api.v2 import helloworld, generate, retry
-from app.api.v3 import generate as generate_v3, retry as retry_v3
+from app.api.v3 import generate, retry, helloworld
 
 router = APIRouter()
 
 router.include_router(helloworld.router, prefix="", tags=["Test"])
-router.include_router(generate.router, prefix="", tags=["GenerateV2"])
-router.include_router(retry.router, prefix="", tags=["RetryV2"])
-router.include_router(generate_v3.router, prefix="/v3", tags=["GenerateV3"])
-router.include_router(retry_v3.router, prefix="/v3", tags=["RetryV3"])
+router.include_router(generate.router, prefix="", tags=["GenerateV3"])
+router.include_router(retry.router, prefix="", tags=["RetryV3"])
